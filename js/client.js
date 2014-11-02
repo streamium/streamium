6f1df8fd6dc1c91f7f0fdcab40afb7166fd2d619
esneider <dariosn@gmail.com>
2014-11-02 18:11:56 -0300

    Client side connection

--- js/client.js
+++ js/client.js
@@ -3,13 +3,9 @@
 var bitcore = require('bitcore');
 var Key = bitcore.Key;
 
-// Compatibility shim
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
 var client = function(room) {
 
-  var peerID = null;
-  var peer = new Peer(peerID, {
+  var peer = new Peer(null, {
     key: 'lwjd5qra8257b9',
     debug: 3,
     config: {
@@ -18,90 +14,40 @@ var client = function(room) {
       }]
     }
   });
-  $(document).ready(function() {
 
-    var k = Key.generateSync();
-    console.log(k.private.toString('hex'));
+  peer.on('close', function() {
+    console.log('Peer connection closed');
+  });
 
+  peer.on('error', function(error) {
+    console.log('Peer connection error: ', error);
+  });
 
-    peer.on('open', function() {
-      $('#my-id').text(peer.id);
+  peer.on('open', function() {
+    console.log('Peer id: ' + peer.id);
+    var connection = peer.connect(room);
+
+    connection.on('open', function() {
+        // connection.close();
     });
-
-    // Receiving a call
-    peer.on('call', function(call) {
-      // Answer the call automatically (instead of prompting user) for demo purposes
-      call.answer(window.localStream);
-      step3(call);
+    connection.on('close', function() {
+      console.log('Initial connection closed.');
     });
-    peer.on('error', function(err) {
-      alert(err.message);
-      // Return to step 2 if error occurs
-      step2();
+    connection.on('error', function(error) {
+      console.log('Initial connection error: ', error);
     });
+  });
 
-    // Click handlers setup
-    $(function() {
-      $('#make-call').click(function() {
-        // Initiate a call!
-        var call = peer.call($('#callto-id').val(), window.localStream);
-
-        step3(call);
-      });
-
-      $('#end-call').click(function() {
-        window.existingCall.close();
-        step2();
-      });
-
-      // Retry if getUserMedia fails
-      $('#step1-retry').click(function() {
-        $('#step1-error').hide();
-        step1();
-      });
-
-      // Get things started
-      step1();
+  peer.on('call', function(connection) {
+    connection.answer();
+    connection.on('stream', function(stream) {
+      $('#video').prop('src', URL.createObjectURL(stream));
     });
-
-    function step1() {
-      // Get audio/video stream
-      navigator.getUserMedia({
-        audio: true,
-        video: true
-      }, function(stream) {
-        // Set your video displays
-        $('#my-video').prop('src', URL.createObjectURL(stream));
-
-        window.localStream = stream;
-        step2();
-      }, function() {
-        $('#step1-error').show();
-      });
-    }
-
-    function step2() {
-      $('#step1, #step3').hide();
-      $('#step2').show();
-    }
-
-    function step3(call) {
-      // Hang up on an existing call if present
-      if (window.existingCall) {
-        window.existingCall.close();
-      }
-
-      // Wait for stream on the call, then set peer video display
-      call.on('stream', function(stream) {
-        $('#their-video').prop('src', URL.createObjectURL(stream));
-      });
-
-      // UI stuff
-      window.existingCall = call;
-      $('#their-id').text(call.peer);
-      call.on('close', step2);
-      $('#step1, #step2').hide();
-      $('#step3').show();
-    }
+    connection.on('close', function() {
+      console.log('Media connection closed');
+    });
+    connection.on('error', function(error) {
+      console.log('Media connection error: ', error);
+    });
   });
 };
