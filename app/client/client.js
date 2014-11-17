a1486f999dad1b5a6451c2d3264f0a84d6f225e9
Manuel Araoz <manuelaraoz@gmail.com>
2014-11-16 22:13:32 -0300

    integrate esteban's lib

--- app/client/client.js
+++ app/client/client.js
@@ -2,17 +2,19 @@
 
 angular.module('streamium.client', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/join/:streamId', {
-    templateUrl: 'client/setup.html',
-    controller: 'ClientSetupCtrl'
-  });
+.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.when('/join/:streamId', {
+      templateUrl: 'client/setup.html',
+      controller: 'ClientSetupCtrl'
+    });
 
-  $routeProvider.when('/join/:streamId/stream', {
-    templateUrl: 'client/stream.html',
-    controller: 'ClientStreamCtrl'
-  });
-}])
+    $routeProvider.when('/join/:streamId/stream', {
+      templateUrl: 'client/stream.html',
+      controller: 'ClientStreamCtrl'
+    });
+  }
+])
 
 .controller('ClientSetupCtrl', function($routeParams) {
   console.log('Params', $routeParams);
@@ -21,3 +23,13 @@ angular.module('streamium.client', ['ngRoute'])
 .controller('ClientStreamCtrl', function($routeParams) {
   console.log('Params', $routeParams);
 });
+
+
+
+var consumer = new channel.Consumer({
+  network: config.network,
+  refundAddress: '2MvmJg8Yb8htySEoAsJTxRQAoPuJmcA7YXW',
+  serverPublicKey: '03d051b328f98ddca19bd8d71b65de64cd5fc815c00aa6a3919e01cd35d15313c8',
+});
+console.log('Funding address: ' + consumer.fundingAddress);
+console.log('Refund address: ' + consumer.refundAddress);
