4cf50848e8c4981aebf22fc1468e84e9ebd767d1
Yemel Jardi <angel.jardi@gmail.com>
2015-01-10 14:57:16 -0300

    Remove home

--- app/provider/provider.js
+++ app/provider/provider.js
@@ -22,7 +22,34 @@ angular.module('streamium.provider', ['ngRoute'])
 ])
 
 .controller('CreateStreamCtrl', function($scope, $location) {
-  console.log('Create Ctrl');
+  $scope.prices = [1, 0.1, 0.01];
+  $scope.stream = {
+    rate: $scope.prices[0]
+  };
+  $scope.stream.name = 'sexybabe69';
+  $scope.stream.address = 'mjhohspVMgcuetHwkH74C2aVKfTdyYdVSP';
+  $scope.stream.rate = 0.1;
+
+  $scope.normalizeName = function() {
+    var name = $scope.stream.name || '';
+    name = name.trim().toLowerCase().replace(/ /g, '-').replace(/\\/g, '-');
+    $scope.stream.name = name;
+  };
+
+  $scope.submit = function() {
+    if (!$scope.form.$valid) return;
+    console.log('Initializing channel');
+    StreamiumProvider.init(
+      $scope.stream.name,
+      $scope.stream.address,
+      $scope.stream.rate,
+      function onCreate(err, done) {
+        if (err) throw err;
+        console.log('DONE');
+        $location.url('/provider');
+        $scope.$apply();
+      });
+  };
 })
 
 .controller('BroadcastStreamCtrl', function($scope, $location, video, StreamiumProvider) {
