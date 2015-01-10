1d722815d5a49e3db88cad97ed73f93b4644433c
Yemel Jardi <angel.jardi@gmail.com>
2015-01-10 13:25:46 -0300

    Add bitcore dependency

--- build-deps.sh
+++ build-deps.sh
@@ -1,1 +1,2 @@
 browserify --standalone channel node_modules/bitcore-channel/index.js > app/bower_components/channel.js
+browserify --standalone bitcore node_modules/bitcore/index.js > app/bower_components/bitcore.js
