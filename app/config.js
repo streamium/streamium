c666ee0ae9443237810c57bb1005a069ad396022
Yemel Jardi <angel.jardi@gmail.com>
2015-01-31 11:07:44 -0300

    Use peerjs servers

--- app/config.js
+++ app/config.js
@@ -4,9 +4,9 @@ var config = {
 
   network: 'testnet',
   peerJS: {
-    key: 'lwjd5qra8257b9',
-    host: '192.168.0.7',
-    port: 9000,
+    key: 'gqjtidom02akyb9',
+    // host: '192.168.0.7',
+    // port: 9000,
     debug: 0,
     config: {
       'iceServers': [{
