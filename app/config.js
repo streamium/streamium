16d2e013f8bcd112efb5ebf445f0db4e51796162
Yemel Jardi <angel.jardi@gmail.com>
2015-01-31 15:43:46 -0300

    Add debug mode with defaults

--- app/config.js
+++ app/config.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var config = {
-
   network: 'testnet',
   peerJS: {
     key: 'gqjtidom02akyb9',
@@ -13,6 +12,12 @@ var config = {
         url: 'stun:stun.l.google.com:19302'
       }]
     }
+  },
+  DEBUG: true,
+  defaults: {
+    providerStream: 'sexybabe69',
+    providerPrivkey: '75d79298ce12ea86863794f0080a14b424d9169f7e325fad52f60753eb072afc',
+    providerAddress: 'n2Py6DKziqwmSWMxN8vdz7tAV6aabK9LCR',
+    providerRate: 0.001
   }
-
 };
