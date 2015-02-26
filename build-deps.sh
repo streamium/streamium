be10decd86737a9271ef7a31a09e47c10bbdfc54
Manuel Araoz <manuelaraoz@gmail.com>
2015-02-26 20:34:47 -0300

    remove unwanted code and files

--- build-deps.sh
+++
@@ -1,2 +0,0 @@
-browserify --standalone channel node_modules/bitcore-channel/index.js > app/bower_components/channel.js
-browserify --standalone bitcore node_modules/bitcore/index.js > app/bower_components/bitcore.js
